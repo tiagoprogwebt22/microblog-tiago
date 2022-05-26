c78ac936c338ca6224debfe1b89b70d213a1e2f4
tiagoprogwebt22 <tiago.progwebt22@gmail.com>
2022-05-26 16:02:16 -0300

    CRUD Usuários (INSERT e SELECT)
    Processos de inserir e listar usuários

--- admin/usuarios.php
+++ admin/usuarios.php
@@ -1,17 +1,19 @@
 <?php 
+require "../inc/funcoes-usuarios.php";
 require "../inc/cabecalho-admin.php"; 
 
-
+$usuarios = lerUsuarios($conexao);
+$quantidade = count($usuarios);
 ?>
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
 		
 		<h2 class="text-center">
-		Usuários <span class="badge badge-primary">X</span>
+		Usuários <span class="badge badge-primary"> <?=$quantidade?> </span>
 		</h2>
 
 		<p class="lead text-right">
-				<a class="btn btn-primary" href="usuario-insere.php">Inserir novo usuário</a>
+			<a class="btn btn-primary" href="usuario-insere.php">Inserir novo usuário</a>
 		</p>
 				
 		<div class="table-responsive">
@@ -27,25 +29,25 @@ require "../inc/cabecalho-admin.php";
 				</thead>
 
 				<tbody>
-
+<?php foreach( $usuarios as $usuario ) { ?>
 					<tr>
-						<td> Nome... </td>
-						<td> E-mail... </td>
-						<td> Tipo... </td>
+						<td> <?=$usuario['nome']?> </td>
+						<td> <?=$usuario['email']?> </td>
+						<td> <?=$usuario['tipo']?> </td>
 						<td class="text-center">
 							<a class="btn btn-warning btn-sm" 
-							href="usuario-atualiza.php">
+							href="usuario-atualiza.php?id=<?=$usuario['id']?>">
 								Atualizar
 							</a>
 						</td>
 						<td class="text-center">
 							<a class="btn btn-danger btn-sm excluir" 
-							href="usuario-exclui.php">
+							href="usuario-exclui.php?id=<?=$usuario['id']?>">
 								Excluir
 							</a>
 						</td>
 					</tr>
-
+<?php } ?>
 				</tbody>                
 			</table>
 	</div>
