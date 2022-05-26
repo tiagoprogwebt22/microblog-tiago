b16f94f44382e83df55fa006f9a8fd0ed582818f
tiagoprogwebt22 <tiago.progwebt22@gmail.com>
2022-05-26 16:58:59 -0300

    CRUD Usuários (SELECT/UPDATE) - início
    Programação para processos de atualização do usuário.

--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -45,7 +45,14 @@ function lerUsuarios(mysqli $conexao):array {
 
 
 // Função lerUmUsuario: usada em usuario-atualiza.php
+function lerUmUsuario(mysqli $conexao, int $id):array {
+    $sql = "SELECT id, nome, email, tipo, senha FROM usuarios
+    WHERE id = $id";
 
+    $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+    return mysqli_fetch_assoc($resultado);
+}
 // fim lerUmUsuario
 
 
@@ -57,7 +64,15 @@ function lerUsuarios(mysqli $conexao):array {
 
 
 // Função atualizarUsuario: usada em usuario-atualiza.php
+function atualizarUsuario(
+    mysqli $conexao, int $id, string $nome, 
+    string $email, string $senha, string $tipo){
+    
+    $sql = "UPDATE usuarios SET nome = '$nome', email = '$email',
+    senha = '$senha', tipo = '$tipo' WHERE id = $id";
 
+    mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+}
 // fim atualizarUsuario
 
 
