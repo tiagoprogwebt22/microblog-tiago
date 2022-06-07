4d9bb9ecf85545c1d8e20931479ace36ab8cb614
tiagoprogwebt22 <tiago.progwebt22@gmail.com>
2022-06-07 16:51:29 -0300

    Correção do exercício

--- admin/posts.php
+++ admin/posts.php
@@ -1,11 +1,19 @@
 <?php 
 require "../inc/cabecalho-admin.php"; 
+require "../inc/funcoes-posts.php";
 
+/* Recuperando os dados do usuário que está logado na sessão */
+$idUsuarioLogado = $_SESSION['id'];
+$tipoUsuarioLogado = $_SESSION['tipo'];
+
+$posts = lerPosts($conexao, $idUsuarioLogado, $tipoUsuarioLogado);
+$quantidade = count($posts);
 ?>      
     
 <div class="row">
   <article class="col-12 bg-white rounded shadow my-1 py-4">
-    <h2 class="text-center">Posts <span class="badge badge-primary">X</span></h2>
+    <h2 class="text-center">Posts 
+      <span class="badge badge-primary"><?=$quantidade?></span></h2>
     <p class="lead text-right">
       <a class="btn btn-primary" href="post-insere.php">Inserir novo post</a>
     </p>
@@ -17,31 +25,37 @@ require "../inc/cabecalho-admin.php";
           <tr>
             <th>Título</th>
             <th>Data</th>
+            <?php if($tipoUsuarioLogado == 'admin'){ ?>
             <th>Autor</th>
+            <?php } ?>
             <th colspan="2" class="text-center">Operações</th>
           </tr>
         </thead>
       
         <tbody>
-
+<?php foreach($posts as $post) { ?>
           <tr>
-            <td> Título do post... </td>
-            <td> 21/12/2112 21:12 </td>
-            <td> Autor do post... </td>
+            <td> <?=$post['titulo']?> </td>
+            <td> <?=$post['data']?> </td>
+            
+            <?php if($tipoUsuarioLogado == 'admin'){ ?>
+            <td> <?=$post['autor']?> </td>
+            <?php } ?>
+
             <td class="text-center">
               <a class="btn btn-warning btn-sm" 
-              href="post-atualiza.php">
+              href="post-atualiza.php?id=<?=$post['id']?>">
                   Atualizar
               </a>
             </td>
             <td class="text-center">
               <a class="btn btn-danger btn-sm excluir"
-              href="post-exclui.php">
+              href="post-exclui.php?id=<?=$post['id']?>">
                   Excluir
               </a>
             </td>
           </tr>
-
+<?php } ?>
         </tbody>                
       </table>
       
